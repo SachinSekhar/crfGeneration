2ca36865bf08c6d13717fa69502bddfc40e8fc64
Sachin Sekhar <sachin.sekhar@gdn-commerce.com>
2021-12-08 10:10:54 +0530

    initial commit

---
+++ crfgeneration/src/main/java/com/codiecon/crfgeneration/crfgeneration/CrfgenerationApplication.java
@@ -0,0 +1,13 @@
+package com.codiecon.crfgeneration.crfgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrfgenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrfgenerationApplication.class, args);
+	}
+
+}
---
+++ crfgeneration/src/test/java/com/codiecon/crfgeneration/crfgeneration/CrfgenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codiecon.crfgeneration.crfgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrfgenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
